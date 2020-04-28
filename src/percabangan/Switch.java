254f3e37470c93d477a6695888010ca46af58995
Maspufatullailiyah <maspufa@gmail.com>
2020-04-27 22:11:54 -0700

    Switch dengan int

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,25 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch (jalanJalan){
+            case 1;
+                System.out.println("Orang Pergi");
+                break;
+             case 2;
+                 System.out.println("Orang Pacaran");
+                 break;
+              case 3;
+                  System.out.println("Yang Ketiga Setan");
+                  break;
+               case 4;
+                   System.out.println("Kasian saya jomblo");
+                   break;
+            default:
+                System.out.println("Mahluk Halus");
+                break;
+
+        }
+    }
+}
